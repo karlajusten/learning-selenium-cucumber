b2a50e1cbb6b9029895c846b0d47c4aa099c558f
Karla Aparecida Justen <justen.karla@gmail.com>
2020-05-23 13:57:08 -0300

    Add timestamp to steps of 'Sign up selenium blog' scenario. It allow the scenario run multiple time.

--- src/test/java/io/cucumber/learning/StepDefinitions.java
+++ src/test/java/io/cucumber/learning/StepDefinitions.java
@@ -6,24 +6,29 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Date;
+
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class StepDefinitions {
 
     WebDriver driver;
+    long timeStamp;
 
     @Given("New user access selenium blog")
     public void new_user_access_selenium_blog() {
         driver = new FirefoxDriver();
         driver.navigate().to("https://selenium-blog.herokuapp.com/signup");
+        Date date= new Date();
+        timeStamp = date.getTime();
     }
 
     @When("New user informs {string}, {string}, {string}")
     public void new_user_informs(String username, String email, String password) {
         SignupPage signup = new SignupPage(driver);
-        signup.enter_username(username);
-        signup.enter_email_address(email);
+        signup.enter_username(username+timeStamp);
+        signup.enter_email_address(timeStamp+email);
         signup.enter_password(password);
         signup.submit_form();
     }
@@ -32,7 +37,7 @@ public class StepDefinitions {
     public void new_user_is_redirected_to_page(String expected_banner_text) {
         UsersPage users = new UsersPage(driver);
         String banner_text = users.get_banner_text();
-        assertEquals(expected_banner_text,banner_text);
+        assertEquals(expected_banner_text+timeStamp,banner_text);
         driver.quit();
     }
 
